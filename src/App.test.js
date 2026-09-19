9aeffeb5779706290f1aa45c48783dc00019c924
rizaltohir55 <rizaltohir55@gmail.com>
2026-09-20 00:05:33 +0700

    replace with permanent redirect to rizaltohir.vercel.app

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders hero title', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Creative Developer/i);
-  expect(linkElement).toBeInTheDocument();
-});
